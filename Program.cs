8118b201c15f7c4fe8381f9d0dbaeffee2378eef
leticialbuk <leticia.albuk@gmail.com>
2023-03-27 23:27:47 -0300

    Adicionando valores a data

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            var data = DateTime.Now;
+
+            Console.WriteLine(data);
+            Console.WriteLine(data.AddDays(12));
+            Console.WriteLine(data.AddMonths(1));
+            Console.WriteLine(data.AddYears(1));
+
+            //var formatada = String.Format("{0:dd/M/yyyy}", data);
+            //Console.WriteLine(formatada);
+        }
+    }
+}
