4a365ef9af8157860a115c3a54899ca281cb27a5
leticialbuk <leticia.albuk@gmail.com>
2023-03-28 16:30:35 -0300

    Utilizando Timezone local e global

---
+++ CultureInfo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace WorkingWithDates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            var utcDate = DateTime.UtcNow;
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(utcDate);
+
+            Console.WriteLine(utcDate.ToLocalTime());
+
+            var timezoneAustralia = TimeZoneInfo.FindSystemTimeZoneById("Pacific/Auckland");
+            Console.WriteLine(timezoneAustralia);
+
+            var horaAustralia = TimeZoneInfo.ConvertTimeFromUtc(utcDate, timezoneAustralia);
+            Console.WriteLine(horaAustralia);
+
+            //var atual = CultureInfo.CurrentCulture;
+            //Console.WriteLine(DateTime.Now.ToString("D", atual));
+
+        }
+
+    }
+}
